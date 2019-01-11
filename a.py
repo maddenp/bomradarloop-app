174d672c67dec1730d285449754c53c370b601d6
Paul Madden <maddenp@colorado.edu>
2019-01-10 23:20:38 -0700

    Fetch radar images from remote

--- a.py
+++ a.py
@@ -32,16 +32,9 @@ radar_interval_sec = radar_interval_min * 60
 ts_now = int(time.time())
 ts_fix = ts_now - (ts_now % radar_interval_sec)
 mkdt = lambda n: dt.datetime.fromtimestamp(ts_fix - (radar_interval_sec * n), tz=dt.timezone.utc)
-strs = [mkdt(n).strftime('%Y%m%d%H%M') for n in range(nimages-1, -1, -1)]
+timestrs = [mkdt(n).strftime('%Y%m%d%H%M') for n in range(nimages, 0, -1)]
 
-# print(dt.datetime.fromtimestamp(ts_now, tz=dt.timezone.utc).strftime('%Y%m%d%H%M'))
-# print(dt.datetime.fromtimestamp(ts_fix, tz=dt.timezone.utc).strftime('%Y%m%d%H%M'))
-# print()
-for x in strs:
-    print(x)
-
-ns = ('0254', '0300', '0306', '0312', '0318', '0324')
 bg = getbg(location)
-merge = lambda bg, fn: np.array(Image.alpha_composite(bg, Image.open(fn).convert('RGBA')))
-images = [merge(bg, f'{radar[location]}.T.20190109{n}.png') for n in ns]
+merge = lambda bg, fg: np.array(Image.alpha_composite(bg, fg))
+images = [merge(bg, getfg(location, timestr)) for timestr in timestrs]
 imageio.mimsave('loop.gif', images, fps=2)
