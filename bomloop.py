7f8391df70ac5afddf8604b1ae81b4fa4aa5a12a
Paul Madden <maddenp@colorado.edu>
2019-01-11 21:22:18 -0700

    Rework imports

--- bomloop.py
+++ bomloop.py
@@ -1,13 +1,11 @@
-# flask
-
-from functools import lru_cache
-from multiprocessing.dummy import Pool
 import datetime as dt
+import functools
 import io
+import multiprocessing.dummy
 import time
 
-from flask import Flask, Response
-from PIL import Image
+import flask
+import PIL.Image
 import requests
 
 nimages = 6
@@ -17,32 +15,32 @@ radar = {
     'Sydney': 'IDR713',
 }
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-@lru_cache()
+@functools.lru_cache()
 def get_bg(location):
     url = get_url(f'products/radar_transparencies/{radar[location]}.background.png')
     return get_image(url)
 
 
-@lru_cache()
+@functools.lru_cache()
 def get_fg(location, time_str):
     url = get_url(f'/radar/{radar[location]}.T.{time_str}.png')
     return get_image(url)
 
 
 def get_image(url):
-    return Image.open(io.BytesIO(requests.get(url).content)).convert('RGBA')
+    return PIL.Image.open(io.BytesIO(requests.get(url).content)).convert('RGBA')
 
 
-@lru_cache()
+@functools.lru_cache()
 def get_frames(location, start):
     bg = get_bg(location)
-    fn = lambda time_str: Image.alpha_composite(bg, get_fg(location, time_str))
-    return Pool(nimages).map(fn, get_time_strs(start))
+    fn = lambda time_str: PIL.Image.alpha_composite(bg, get_fg(location, time_str))
+    return multiprocessing.dummy.Pool(nimages).map(fn, get_time_strs(start))
 
 
-@lru_cache()
+@functools.lru_cache()
 def get_loop(location, start):
     loop = io.BytesIO()
     frames = get_frames(location, start)
@@ -56,7 +54,7 @@ def get_loop(location, start):
     return loop.getvalue()
 
 
-@lru_cache()
+@functools.lru_cache()
 def get_time_strs(start):
     mkdt = lambda n: dt.datetime.fromtimestamp(start - (radar_interval_sec * n), tz=dt.timezone.utc)
     return [mkdt(n).strftime('%Y%m%d%H%M') for n in range(nimages, 0, -1)]
@@ -71,4 +69,4 @@ def main():
     location = 'Sydney'
     now = int(time.time())
     start = now - (now % radar_interval_sec)
-    return Response(get_loop(location, start), mimetype='image/jpeg')
+    return flask.Response(get_loop(location, start), mimetype='image/jpeg')
